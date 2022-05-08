b564fa02daaf0b1addddb61c7cd7f7de2e62415d
Nurbolot <nurbolot.boobekovv@gmail.com>
2022-05-08 22:19:22 +0300

    fixed bug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,7 +14,8 @@ const Nav = ({ onToggleActiveDay, data, active }) => {
          {days.map(({ day, ru }, idx) => {
             const activeDays = data.filter(item => item.day === day)
             const completedDays = activeDays.filter(item => item.completed).length;
-            const activeDaysPercent = (completedDays * 100) / activeDays.length;
+            const activeDaysPercent = activeDays.length > 0 ?
+               (completedDays * 100) / activeDays.length : 0;
 
             const btnClass = active === day ? "nav__link nav__link-active" : "nav__link";
 
